89ce28018913a2fcbb554fa4869b4eeccc696f00
Abiodun001-world <adekunleabiodun354@gmail.com>
2023-09-05 12:50:43 +0100

    included code snippets and explanation for each section like Creating Functions to Generate and Play Keys.included screenshot demonstrating the demo. Once complete, do add the code and details in readme

---
+++ script.js
@@ -0,0 +1,28 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key")
+
+let allKeys = [],
+audio = new Audio(`tones/a.mp3`);
+const playTune = (key) => {
+    audio.src = `tones/${key}.mp3`; // passing audio src based on key pressed 
+    audio.play(); // playing audio
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`); // getting clicked key element
+    clickedKey.classList.add("active"); // adding active class to the clicked key element
+    setTimeout(() => { // removing active class after 150 ms from the clicked key element
+        clickedKey.classList.remove("active");
+    }, 150);
+}
+
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key); // adding data-key value to the allKeys array
+    // calling playTune function with passing data-key value as an argument
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+
+const pressedKey = (e) => {
+    // if the pressed key is in the allKeys array, only call the playTune function
+    if(allKeys.includes(e.key)) playTune(e.key);
+}
+
+document.addEventListener("keydown", pressedKey);
